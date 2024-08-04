fbc409e4a677249c984b2d4eaf8bb20b0c2f0f7c
Vitaliy Ukrainskiy <vitaliy.ukrainskiy@gameloft.com>
2024-08-04 16:35:52 +0300

    update path

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,9 +1,9 @@
 const API_PATHS = {
   product: "https://nfewdukm7g.execute-api.eu-west-1.amazonaws.com/prod",
-  order: "https://8h5j7amm4k.execute-api.eu-west-1.amazonaws.com/prod",
+  order: "https://ukrainskyi-vitalii-cart-api-dev.eu-west-1.elasticbeanstalk.com",
   import: "https://93lnx6q689.execute-api.eu-west-1.amazonaws.com/prod",
   bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  cart: "https://8h5j7amm4k.execute-api.eu-west-1.amazonaws.com/prod",
+  cart: "https://ukrainskyi-vitalii-cart-api-dev.eu-west-1.elasticbeanstalk.com",
 };
 
 export default API_PATHS;
