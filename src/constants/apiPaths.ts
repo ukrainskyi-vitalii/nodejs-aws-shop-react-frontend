6a324ce690527b6b0b4afca79dab6fe2c5c11728
Vitaliy Ukrainskiy <vitaliy.ukrainskiy@gameloft.com>
2024-08-11 20:43:17 +0300

    update bff path

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -2,7 +2,7 @@ const API_PATHS = {
   product: "https://nfewdukm7g.execute-api.eu-west-1.amazonaws.com/prod",
   order: "https://ukrainskyi-vitalii-cart-api-dev.eu-west-1.elasticbeanstalk.com",
   import: "https://93lnx6q689.execute-api.eu-west-1.amazonaws.com/prod",
-  bff: "http://ukrainskyi-vitalii-bff-api-alpha.eu-west-1.elasticbeanstalk.com",
+  bff: "http://ukrainskyi-vitalii-bff-api-test.eu-west-1.elasticbeanstalk.com",
   cart: "https://ukrainskyi-vitalii-cart-api-dev.eu-west-1.elasticbeanstalk.com",
 };
 
